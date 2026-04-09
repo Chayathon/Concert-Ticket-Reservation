7f0c0515597b10ad23f854a3ae3f5bcaf485393f
Chayathon <gusgus157903@gmail.com>
2026-04-09 11:20:19 +0700

    Add Config modules, enable CORS, use dotenv

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConcertModule } from './concert/concert.module';
 import { ReservationModule } from './reservation/reservation.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConcertModule, ReservationModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    ConcertModule,
+    ReservationModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
